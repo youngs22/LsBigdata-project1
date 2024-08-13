cc95e1bfd69012c5798ee8e1db27649952fe4667
Youna Park <doobu6675@gmail.com>
2024-08-13 09:56:09 +0900

    8/13 오전

---
+++ code/plotly 교재/subplot.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from palmerpenguins import load_penguins
+
+penguins = load_penguins()
+# penguins.head()
+
+fig = px.scatter(
+    penguins,
+    x="bill_length_mm",
+    y="bill_depth_mm",
+    color="species",
+    # trendline="ols" # p.134
+)
+fig.show()
+
+from plotly.subplots import make_subplots
+
+fig_subplot = make_subplots(
+    rows=3, cols=3,
+    subplot_titles=("Adeli","Gentoo","Chinstrap")
+)
+
+fig_subplot.add_trace(
+    {
+    "type":"scatter",
+    "mode":"markers",
+    "x":penguins.query("species=='Adelie'")["bill_length_mm"],
+    "y":penguins.query("species=='Adelie'")["bill_depth_mm"],
+    "name":"Adeli"
+    },
+row=1, col=1
+)
+
+fig_subplot.add_trace(
+    {
+    "type":"scatter",
+    "mode":"markers",
+    "x":penguins.query("species=='Gentoo'")["bill_length_mm"],
+    "y":penguins.query("species=='Gentoo'")["bill_depth_mm"],
+    "name":"Gentoo"
+    },
+row=1, col=2
+)
+
+fig_subplot.add_trace(
+    {
+    "type":"scatter",
+    "mode":"markers",
+    "x":penguins.query("species=='Chinstrap'")["bill_length_mm"],
+    "y":penguins.query("species=='Chinstrap'")["bill_depth_mm"],
+    "name":"Chinstrap"
+    },
+row=1, col=3
+)
+
+fig_subplot.update_layout(
+    title=dict(text="펭귄종별 부리 길이 vs 깊이",
+               x=0.5)
+)
